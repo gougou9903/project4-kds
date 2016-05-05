dc99d4bcd062a8ff17f3579f38e73e38d7098820
RayLiu-1 <ruli7792@colorado.edu>
2016-05-04 23:00:38 -0600

    Update nRF24L01.h

--- SPI_beagleBone/nRF24L01.h
+++ SPI_beagleBone/nRF24L01.h
@@ -9,5 +9,8 @@ extern uint8_t csn_pin;
 extern uint8_t ce_pin;
 
 void scn(int mode);
+uint8_t read_register(uint8_t reg);
+uint8_t read_register(uint8_t reg, uint8_t* buf, uint8_t len);
+uint8_t write_register(uint8_t reg, uint8_t value);
 
 #endif

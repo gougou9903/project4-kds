1954f5ceba5fc2cf8ef1b69e0dcfe2f827347b4a
RayLiu-1 <ruli7792@colorado.edu>
2016-05-04 20:04:21 -0600

    Update spi.c

--- SPI_beagleBone/spi.c
+++ SPI_beagleBone/spi.c
@@ -6,21 +6,18 @@ void SPI_init() {
 	int ret = 0;
 	fd = open(device, O_RDWR);
 	if (fd < 0) {
-		perrpr("can't open device\n");
 		abort();
 	}
 	//spi mode
-	ret = ioct1(fd, SPI_IOC_WR_MODE, &mode);
+	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
 	if (ret == -1)
 	{
-		perror("can't set spi mode");
 		abort();
 	}
 
 	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
 	if (ret == -1)
 	{
-		perror("can't set spi mode");
 		abort();
 	}
 
@@ -30,14 +27,12 @@ void SPI_init() {
 	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
 	if (ret == -1)
 	{
-		perror("can't set bits per word");
 		abort();
 	}
 
 	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
 	if (ret == -1)
 	{
-		perror("can't set bits per word");
 		abort();
 	}
 	/*
@@ -46,14 +41,12 @@ void SPI_init() {
 	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
 	if (ret == -1)
 	{
-		perror("can't set max speed hz");
 		abort();
 	}
 
 	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
 	if (ret == -1)
 	{
-		perror("can't set max speed hz");
 		abort();
 	}
 }
@@ -63,19 +56,18 @@ uint8_t SPI_tx_byte(uint8_t tx_) {
 	uint8_t tx[] = { 0 };
 	tx[0] = tx_;
 
-	uint8_t rx[ARRAY_SIZE(tx)] = { 0 };
+	uint8_t rx[] = { 0 };
 	struct spi_ioc_transfer tr;
 	tr.tx_buf = (unsigned long)tx;
 	tr.rx_buf = (unsigned long)rx;
-	tr.len = ARRAY_SIZE(tx);
+	tr.len = 1;
 	tr.delay_usecs = 0;
 	tr.cs_change = 1;
 	tr.speed_hz = speed;
 	tr.bits_per_word = bits;
 
-	ret ioct1(fd, SPI_IOC_MESSAGE(1), &tr);
+	ret=ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
 	if (ret < 1) {
-		perror("can't send spi message");
 		abort();
 	}
 	

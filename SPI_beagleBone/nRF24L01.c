dfbc86cb5a3f744914d3136679de3d7ce05d412f
RayLiu-1 <ruli7792@colorado.edu>
2016-05-04 18:46:40 -0600

    Update nRF24L01.c

--- SPI_beagleBone/nRF24L01.c
+++ SPI_beagleBone/nRF24L01.c
@@ -2,4 +2,28 @@
 #include "nRF24L01.h"
 #include "spi.h"
 
-void
+void csn(int mode)
+{
+	digitalWrite(csn_pin, mode);
+}
+
+void ce(int level) {
+	digitalWrite(ce_pin, level);
+}
+
+uint8_t read_register(uint8_t reg) {
+	csn(LOW);
+	spi->transfer(reg);
+	uint8_t result = SPI_tx_byte(0xff);
+	csn(HIGH);
+	return result;
+}
+
+uint8_t write_register(uint8_t reg, uint8_t value) {
+	uint8_t status;
+	csn(LOW);
+	status = SPI_tx_byte(reg);
+	csn(HIGH);
+
+	return status;
+}

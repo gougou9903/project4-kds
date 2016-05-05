a517824d0601311f8a0c3f37192b8c283a4ddc53
RayLiu-1 <ruli7792@colorado.edu>
2016-05-04 20:13:55 -0600

    Update nRF24L01.h

--- SPI_beagleBone/nRF24L01.h
+++ SPI_beagleBone/nRF24L01.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef _NRF24L01_
+#define _NRF24L01_
 
 #include "spi.h"
 #include "Nordic.h"
@@ -8,3 +9,5 @@ uint8_t csn_pin=115;
 uint8_t ce_pin=117;
 
 void scn(int mode);
+
+#endif
